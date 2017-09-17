8ed3323980902cd6521e198565e504befcdf1378
amare16 <amarenighatu16@gmail.com>
2017-09-17 17:46:13 +0200

    change url localhost to https://stormy-dusk-56638.herokuapp.com in config/app.php file

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://stormy-dusk-56638.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
